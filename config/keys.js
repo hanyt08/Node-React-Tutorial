20a936503bd68a0e818bb692e8b1c13841986ff5
Yuting Han <hanyt08@gmail.com>
2019-07-05 15:23:38 +0800

    google strategy setting

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = ({
-    googleClientID: '147221044536-rs2geu69g6ioeu7tn61evuu1vqnb4db7.apps.googleusercontent.com',
-    googleClientSecret: 'p5lpHvsvgnYa6N4edqu1Uix2',
+    googleClientID: '147221044536-3mivhndmu0c6smhpk27d2rofkjqre3rn.apps.googleusercontent.com',
+    googleClientSecret: 'diYh3b2BFqUPWJ6ddtlJ8kL6',
     mongoURI:'mongodb+srv://hanyt08:V0sR3jVkUVV2xMoU@emaily-zk92i.mongodb.net/test?retryWrites=true&w=majority',
     cookieKey: 'redacted'
   })
