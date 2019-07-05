d56052d0544f96229c7591c2bd61afcfadda5ff7
Yuting Han <hanyt08@gmail.com>
2019-07-05 15:00:04 +0800

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = ({
-  googleClientID: '147221044536-rs2geu69g6ioeu7tn61evuu1vqnb4db7.apps.googleusercontent.com',
-  googleClientSecret: 'p5lpHvsvgnYa6N4edqu1Uix2',
-  mongoURI:'mongodb+srv://hanyt08:V0sR3jVkUVV2xMoU@emaily-zk92i.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: 'redacted'
-})
+    googleClientID: '147221044536-rs2geu69g6ioeu7tn61evuu1vqnb4db7.apps.googleusercontent.com',
+    googleClientSecret: 'p5lpHvsvgnYa6N4edqu1Uix2',
+    mongoURI:'mongodb+srv://hanyt08:V0sR3jVkUVV2xMoU@emaily-zk92i.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'redacted'
+  })
